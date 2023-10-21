616a1d2a20c98de524d6a2204a542dff8f099bc7
Omkar Sharma <omkarsharma2821@gmail.com>
2023-10-21 11:38:10 +0530

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,13 +4,124 @@ import React from "react";
 
 const Home = () => {
   // const myStyle={backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh' }
-  const myStyle={backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh', width: '100%'}
+  // const myStyle={backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh', width: '100%'}
 
   return (
-    <div className="vh-100">
-      <img src="images/bg.jpg" style={myStyle} alt="" />
+    <>
+    <div className=" vh-100 home-bg ">
+      {/* <img src="" style={myStyle} alt="" /> */}
     </div>
-
+    <div>
+    <section>
+  <div className="container py-5">
+    <div className="row">
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G1.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G2.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G3.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <section>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G1.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G2.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="card">
+            <img
+              className="img-fluid"
+              src="images/G3.gif"
+              alt=""
+            />
+            <button className="btn btn-primary mt-3 mx-4">Add to Cart</button>
+            <button className="btn btn-primary mt-3 mx-4">Buy Now</button>
+            <h4 className="text-center mt-4" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+    </>
   );
 };
 export default Home;
