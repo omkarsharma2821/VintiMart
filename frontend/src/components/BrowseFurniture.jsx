a6c70440545b6de76510adb74dd22ba827e6c8f1
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-13 11:20:06 +0530

    offcanvas added

--- frontend/src/components/BrowseFurniture.jsx
+++ frontend/src/components/BrowseFurniture.jsx
@@ -7,7 +7,7 @@ const BrowseFurniture = () => {
   const [productsArray, setProductsArray] = useState([]);
   const [FurnitureList, setFurnitureList] = useState([]);
 
-  const {addItemToCart} = useCartContext();
+  const { addItemToCart } = useCartContext();
 
   const getFurnitureData = async () => {
     const res = await fetch('http://localhost:5000/furniture/getall');
@@ -16,14 +16,14 @@ const BrowseFurniture = () => {
     console.table(data);
     setFurnitureList(data);
     setProductsArray(data)
-}
+  }
 
-useEffect(() => {
-  getFurnitureData()
-}, [])
+  useEffect(() => {
+    getFurnitureData()
+  }, [])
 
 
-  const brands = ["pepperfry", "urban ladder", "godrej", "durian","wood mount", "royal oak","zuari", "damro"];
+  const brands = ["pepperfry", "urban ladder", "godrej", "durian", "wood mount", "royal oak", "zuari", "damro"];
   const searchProduct = (e) => {
     const search = e.target.value;
     let filteredData = FurnitureList.filter((product) => {
@@ -34,7 +34,7 @@ useEffect(() => {
 
   const filterBrand = (e) => {
     const search = e.target.value;
-    if(!search) {
+    if (!search) {
       setProductsArray([...FurnitureList]);
       return;
     }
@@ -42,7 +42,7 @@ useEffect(() => {
     let filteredData = FurnitureList.filter((product) => {
       return product.brand.toLowerCase() === search.toLowerCase();
     });
-    setProductsArray(filteredData); 
+    setProductsArray(filteredData);
   };
 
   return (
@@ -50,60 +50,77 @@ useEffect(() => {
       <header className="bg-dark">
         <div className="container py-3">
           <h1 className="text-center display-2 mb-0 text-white ">
-          Discover Your Style
+            Discover Your Style
           </h1>
-            <h5 className='text-white text-center mb-4'>Home Decor for Sale - Place Where Comfort Meets Elegance</h5>
-            <div className='input-group  d-flex mx-auto w-50 mb-2'>
-          <input
-            type="text"
-            className='form-control m-auto'
-            onChange={searchProduct}
-            placeholder='Search in VintiMart....'
-          />
-          <button className="btn btn-primary"><i class="fa-solid fa-magnifying-glass fa-beat fa-lg"></i></button>
+          <h5 className='text-white text-center mb-4'>Home Decor for Sale - Place Where Comfort Meets Elegance</h5>
+          <div className='input-group  d-flex mx-auto w-50 mb-2'>
+            <input
+              type="text"
+              className='form-control m-auto'
+              onChange={searchProduct}
+              placeholder='Search in VintiMart....'
+            />
+            <button className="btn btn-primary"><i class="fa-solid fa-magnifying-glass fa-beat fa-lg"></i></button>
           </div>
         </div>
       </header>
       <div className="px-5">
-      <div className="row mt-4">
-        <div className="col-3">
-          <div className="card shadow">
-            <div className="card-body">
-              <h6 className='text-muted text-center'>Apply Filter</h6>
-              <hr />
-              <select className="form-control" onChange={filterBrand}>
-                <option className='text-center' value="">Select Brand</option>
-                {brands.map((b) => (
-                  <option value={b}>{b}</option>
-                ))}
-              </select>
+        <div className="row mt-4">
+          <div className="col-3">
+            <div className="card shadow">
+              <div className="card-body">
+                <h6 className='text-muted text-center'>Apply Filter</h6>
+                <hr />
+                <select className="form-control" onChange={filterBrand}>
+                  <option className='text-center' value="">Select Brand</option>
+                  {brands.map((b) => (
+                    <option value={b}>{b}</option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-9">
-          <div className="conatiner-fluid">
-            {/* <h1>Product List</h1> */}
-            <div className="row">
-              {productsArray.map((product) => (
-                <div className="col-md-3 mb-4">
-                  <div className="card shadow" style={{ overflow: "hidden" }}>
-                    <img src={'http://localhost:5000/'+product.image} alt="" className="my-card-img" />
-                    <div className="card-body">
-                      <p className="text-muted">
-                        {product.sponsored ? "sponsored" : ""}
-                      </p>
-                      <h5><i class="fa-solid fa-tag px-1"></i>{product.brand}</h5>
-                      <h5><i className="fa-solid fa-rupee-sign fa-fade mx-1" style={{color: "#000000"}}></i>{product.price}</h5>
-                      <button onClick={() => {addItemToCart(product)}} className="btn btn-sm btn-primary button">Add to Cart</button>
-                      <button onClick={() => {addItemToCart(product)}} className="btn btn-sm btn-primary mx-2 button">Buy Now</button>
+          <div className="col-9">
+            <div className="conatiner-fluid">
+              {/* <h1>Product List</h1> */}
+              <div className="row">
+                {productsArray.map((product) => (
+                  <div className="col-md-3 mb-4">
+                    <div className="card shadow" style={{ overflow: "hidden" }}>
+                      <img src={'http://localhost:5000/' + product.image} alt="" className="my-card-img" />
+                      <div className="card-body">
+                        <p className="text-muted">
+                          {product.sponsored ? "sponsored" : ""}
+                        </p>
+                        <h5><i class="fa-solid fa-tag px-1"></i>{product.brand}</h5>
+                        <h5><i className="fa-solid fa-rupee-sign fa-fade mx-1" style={{ color: "#000000" }}></i>{product.price}</h5>
+                        <button onClick={() => { addItemToCart(product) }} className="btn btn-sm btn-primary button">Add to Cart</button>
+                        {/* <button onClick={() => {addItemToCart(product)}} className="btn btn-sm btn-primary mx-2 button">Buy Now</button> */}
+                        <button class="btn btn-primary btn-sm mx-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+                          Buy Now
+                        </button>
+
+                        <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+                          <div class="offcanvas-header">
+                            <h1 class="offcanvas-title display-3" id="staticBackdropLabel">VintiMart</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                          </div>
+                          <div class="offcanvas-body">
+                            <h3>
+                            This Furniture is currently unavilable we will notify you once it comes in stock.
+                            <br />
+                            Thank you!
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   )
