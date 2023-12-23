31df9c2e17cbf926d76f36d02fae6711fbf7aa50
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-23 16:51:26 +0530

    not found page corrected

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const NotFound = () => {
   return (
     <>
-        <h1 className='text-muted text-center mt-5'>Oops! Something Went Wrong </h1>
-        <div className='container error vh-100' ></div>
+    <div className='card container bg-light shadow-lg mt-5 w-50'>
+      <div className="mt- bg-light">
+        <h1 className='text-muted text-center my-2'>Oops! Something Went Wrong </h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <img className='error' src="https://www.cloudconsult.ca/public/no-search-found.png" alt="PAGE NOT FOUND IMAGE" />
+        </div>
+        </div>
     </>
   ) 
 }
